ab0ea6c19cf7d75941615f26b96509fd59be69c4
manminder11 <ManminderSingh@edu.sait.ca>
2025-09-21 20:43:11 -0600

    some tweaks fixed

--- src/app/createItem/page.js
+++ src/app/createItem/page.js
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import ImageUploading from "../addingpictures/page";
+import ImageUploading from "../addingimages/page";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
